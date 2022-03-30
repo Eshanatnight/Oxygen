72bf4a9c26aed1525d929b57cee0ecc8ba25dee1
Eshan Chatterjee <eshan2001221@gmail.com>
2022-03-31 02:16:01 +0530

    Update to database module

--- src/db.rs
+++ src/db.rs
@@ -1,8 +1,24 @@
 use color_eyre::eyre::Result;
-use rusqlite::{Connection, params};
+use rusqlite::{Connection, params, types::Type};
 use crate::audio_clip::AudioClip;
+use chrono::prelude::*;
 
 pub struct Db(Connection);
+
+/*
+    * For the `list` command, we can send a bunch of AudioClip entities to the console.
+    * but that won't be very efficient or useful even cause we are not going to use them then and there.
+    * `list` will mostly be used to load the name of the file to play.
+    * so maybe sending a different struct is better, that some how will hold the name of the clips.
+
+    * this struct just contains the metadata for the clip.
+*/
+pub struct ClipMeta
+{
+    pub clip_id: usize,
+    pub clip_name: String,
+    pub clip_date: DateTime<Utc>,
+}
 
 
 fn encode(samples: &[f32]) -> Vec<u8>
@@ -17,6 +33,28 @@ fn encode(samples: &[f32]) -> Vec<u8>
     buf
 }
 
+fn decode(bytes: &[u8]) -> Vec<f32>
+{
+    let mut samples = Vec::with_capacity(bytes.len() / 4);
+
+    /*
+    // This might work idk
+    for i in 0..bytes.len() / 4
+    {
+        let mut sample = [0u8; 4];
+        sample.copy_from_slice(&bytes[i * 4..(i + 1) * 4]);
+        samples.push(f32::from_be_bytes(sample));
+    }
+    */
+
+    for chunk in bytes.chunks(4)
+    {
+        samples.push(f32::from_be_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]));
+    }
+
+    samples
+}
+
 
 impl Db
 {
@@ -24,7 +62,8 @@ impl Db
     pub fn open() -> Result<Self>
     {
         // the sqlitefile will be stored in a directory named "data" up the src dir
-        // if we consider the binary is in a directory called "bin" and there is also a directory called "data"
+        // if we consider the binary is in a directory called "bin" and
+        // there is also a directory called "data"
         // then the sqlite file will be stored in "data/db.sqlite"
         /*
             Directory structure:
@@ -78,4 +117,101 @@ impl Db
 
         Ok(())
     }
+
+
+    pub fn load(&self, name: &str) -> Result<Option<AudioClip>>
+    {
+        let mut stmt = self
+        .0
+        .prepare
+        (
+            "
+            SELECT id, name, date, sample_rate, samples
+            FROM clips
+            WHERE name = ?1
+            "
+        )?;
+
+        let mut clip_iter = stmt.query_map([name], |row|
+            {
+                let _date: String = row.get(2)?;  // we need to convert this into a `DateTime` type
+                let samples: Vec<u8> = row.get(4)?;
+
+                Ok(AudioClip
+                {
+                    id: Some(row.get(0)?),
+                    name: row.get(1)?,
+                    date: _date.parse().map_err(|_|
+                        {
+                            rusqlite::Error::InvalidColumnType(2, "date".to_string(), Type::Text)
+                        })?,
+                    sample_rate: row.get(3)?,
+                    samples: decode(&samples),
+                })
+            })?;
+
+        // Basically we will check if our iterator is empty or no
+        // i.e. if it has a audio clip or not, if it is there return it
+        // else return None
+        Ok(if let Some(clip) = clip_iter.next()
+        {
+            Some(clip?)
+        }
+
+        else
+        {
+            None
+        })
+    }
+
+
+    // so this would retrive the information of the clips, just the basic Info
+    // like name, id and date. Right now this does not take any args
+    // but maybe i will add something like a filter later.
+    pub fn list(&self) -> Result<Vec<ClipMeta>>
+    {
+        let mut stmt = self
+        .0
+        .prepare
+        (
+            "
+            SELECT id, name, date
+            FROM clips
+            ORDER BY date
+            "
+        )?;
+
+        let clip_iter = stmt.query_map([], |row|
+            {
+                let _date: String = row.get(2)?;  // we need to convert this into a `DateTime` type
+
+                Ok(ClipMeta
+                {
+                    clip_id: row.get(0)?,
+                    clip_name: row.get(1)?,
+                    clip_date: _date.parse().map_err(|_|
+                        {
+                            rusqlite::Error::InvalidColumnType(2, "date".to_string(), Type::Text)
+                        })?,
+                })
+            })?;
+
+            Ok(clip_iter.collect::<Result<_, rusqlite::Error>>()?)
+    }
+
+
+    pub fn delete(&self, name: &str) -> Result<()>
+    {
+
+        self.0.execute(
+            "
+            DELETE FROM clips
+            WHERE name = ?1
+            ",
+            params![name]
+        )?;
+
+        Ok(())
+    }
 }
+
